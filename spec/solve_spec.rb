616fd660c687d99864881930674f8d0267f83bb8
imubarak234 <imubarak2424@gmail.com>
2022-03-11 15:13:47 +0100

    fizzbuzz test cases

--- spec/solve_spec.rb
+++ spec/solve_spec.rb
@@ -34,4 +34,40 @@ describe 'Testing the reverse method in the solver class' do
 
 end
 
+describe 'Testing the fizzbuzz method in the solver class' do 
+  
+  it 'The method fizzbuzz should return fizz given arguments 3'
+    expect(@solver.fizzbuzz(3)).to eq "fizz"
+  end
+
+  it 'The method fizzbuzz should return fizz given arguments 9'
+    expect(@solver.fizzbuzz(9)).to eq "fizz"
+  end
+
+  it 'The method fizzbuzz should return buzz given the argument 5' do
+    expect(@solver.fizzbuzz(5)).to eq "buzz"
+  end
+
+  it 'The method fizzbuzz should return buzz given the argument 10' do
+    expect(@solver.fizzbuzz(10)).to eq "buzz"
+  end
+
+  it 'The method fizzbuzz should return fizzbuzz given the argument 15' do
+    expect(@solver.fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'The method fizzbuzz should return buzz given the argument 30' do
+    expect(@solver.fizzbuzz(30)).to eq "fizzbuzz"
+  end
+
+  it 'The method fizzbuzz should return "7" given the argument 7' do
+    expect(@solver.fizzbuzz(7)).to eq "7"
+  end
+
+  it 'The method fizzbuzz should return "11" given the argument 11' do
+    expect(@solver.fizzbuzz(11)).to eq "11"
+  end
+
+end
+
 
