1ffb215336b1072468827f7eec1a3c9d2cb5166c
imubarak234 <imubarak2424@gmail.com>
2022-03-11 14:51:24 +0100

    factorial testcases

---
+++ spec/solve_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../solver.rb'
+
+describe 'Testing the factoral method in the solver class' do
+  before :each do
+    @solver = Solver.new
+  end
+
+  it 'with the above parameter a Solver object should be created' do
+    @Solver.should be_an_instance_of Solver
+  end
+
+  it 'return the appropriate output with 4 as an argument' do
+    @Solver.factorial(4).should eql 24
+  end
+
+  it 'the special case input 0 should return a 1' do
+    @solver.factorial(0) eql 1
+  end
+
+  it 'the method should only accept positive integers and 0' do
+    expect {@solver.factoral}.to raise_error("Not a positive integer")
+  end
+end
